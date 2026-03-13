90beb14341459e66df2a97016d73108f1fba9c3d
imxantek <antekaminski@gmail.com>
2026-03-13 17:04:56 +0100

    Commit adding methods to main.py. We have numMembers displaying the number of people in our group, welcomeMessage welcoming the user and formatInfo displaying data into the console

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
-groupName="Majchrowski_Kaminski"
-print(groupName)
-groupList={"Majchrowski", "Kaminski"}
-print(groupList)
-print("Program compiled without errors")
+def numMembers(list):
+    return len(list)
+def welcomeMessage():
+    print("=== Welcome to our project group! ===")
+def formatInfo():
+    welcomeMessage()
+    groupName = "Majchrowski_Kaminski"
+    print(f"Group Name: {groupName}")
+    groupList = {"Majchrowski", "Kaminski"}
+    print(f"Group List: {groupList}")
+    print(f"Number of members: {numMembers(groupList)}")
+    print("Program compiled without errors")
+    print("=== End of program ===")
+if __name__=="__main__":
+    formatInfo()
